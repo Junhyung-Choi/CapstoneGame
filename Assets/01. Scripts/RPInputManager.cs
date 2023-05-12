259054e3fdc8256dd3d5eb803435533e82df6d7b
Junhyung-Choi <junhyung9571@naver.com>
2023-05-12 11:06:04 +0900

    Chore: Testing Scene

--- Assets/01. Scripts/RPInputManager.cs
+++ Assets/01. Scripts/RPInputManager.cs
@@ -33,7 +33,7 @@ public class RPInputManager : MonoBehaviour
     void Start()
     {
         try{
-            sp.PortName = "/dev/cu.usbmodem21201";     // 여기에는 아두이노 포트 넣어주면 됩니다.
+            sp.PortName = "/dev/cu.usbserial-1120";     // 여기에는 아두이노 포트 넣어주면 됩니다.
             sp.BaudRate = 9600;      // 아두이노 보레이트랑 맞춰주시면 됩니다.
             sp.DataBits = 8;
             sp.Parity = Parity.None;
@@ -56,15 +56,17 @@ public class RPInputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputMatrix[0,0] = getSensorValue(0);
-        inputMatrix[0,1] = getSensorValue(1);
-        inputMatrix[0,2] = getSensorValue(2);
-        inputMatrix[0,3] = getSensorValue(3);
+        inputMatrix[1,0] = getSensorValue(0);
+        inputMatrix[0,0] = getSensorValue(1);
+        inputMatrix[1,1] = getSensorValue(2);
+        inputMatrix[0,1] = getSensorValue(3);
 
-        inputMatrix[1,0] = getSensorValue(4);
-        inputMatrix[1,1] = getSensorValue(5);
-        inputMatrix[1,2] = getSensorValue(6);
-        inputMatrix[1,3] = getSensorValue(7);
+        inputMatrix[1,2] = getSensorValue(4);
+        inputMatrix[0,2] = getSensorValue(5);
+        inputMatrix[1,3] = getSensorValue(6);
+        inputMatrix[0,3] = getSensorValue(7);
+
+        print(inputMatrix[0,0] + " " + inputMatrix[1,0] + " " + inputMatrix[0,1] + " " + inputMatrix[1,1] + " " + inputMatrix[0,2] + " " + inputMatrix[1,2] + " " + inputMatrix[0,3] + " " + inputMatrix[1,3] + " ");
 
         if(isViewerOpen)
         {
@@ -81,25 +83,25 @@ public class RPInputManager : MonoBehaviour
                 else return data.datas[0];
             case 1:
                 if(Input.GetKey(KeyCode.R)) return 1;
-                return 0;
+                else return data.datas[1];
             case 2:
                 if(Input.GetKey(KeyCode.Y)) return 1;
-                return 0;
+                else return data.datas[2];
             case 3:
                 if(Input.GetKey(KeyCode.I)) return 1;
-                return 0;
+                else return data.datas[3];
             case 4:
                 if(Input.GetKey(KeyCode.X)) return 1;
-                return 0;
+                else return data.datas[4];
             case 5:
                 if(Input.GetKey(KeyCode.V)) return 1;
-                return 0;
+                else return data.datas[5];
             case 6:
                 if(Input.GetKey(KeyCode.N)) return 1;
-                return 0;
+                else return data.datas[6];
             case 7:
                 if(Input.GetKey(KeyCode.Comma)) return 1;
-                return 0;
+                else return data.datas[7];
         }
         return 1;
     }
@@ -110,15 +112,15 @@ public class RPInputManager : MonoBehaviour
         if(canvas.gameObject.activeSelf == false) canvas.gameObject.SetActive(true);
         Transform inputViewer = canvas.GetChild(0);
 
-        inputViewer.GetChild(0).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,0]),0,0);
-        inputViewer.GetChild(1).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,1]),0,0);
-        inputViewer.GetChild(2).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,2]),0,0);
-        inputViewer.GetChild(3).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,3]),0,0);
+        inputViewer.GetChild(0).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,0] * 0.05f),0,0);
+        inputViewer.GetChild(1).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,1] * 0.05f),0,0);
+        inputViewer.GetChild(2).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,2] * 0.05f),0,0);
+        inputViewer.GetChild(3).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[0,3] * 0.05f),0,0);
 
-        inputViewer.GetChild(4).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,0]),0,0);
-        inputViewer.GetChild(5).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,1]),0,0);
-        inputViewer.GetChild(6).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,2]),0,0);
-        inputViewer.GetChild(7).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,3]),0,0);
+        inputViewer.GetChild(4).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,0] * 0.05f),0,0);
+        inputViewer.GetChild(5).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,1] * 0.05f),0,0);
+        inputViewer.GetChild(6).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,2] * 0.05f),0,0);
+        inputViewer.GetChild(7).GetComponent<Image>().color = new Color(Math.Abs(inputMatrix[1,3] * 0.05f),0,0);
     }
 
     private void OnApplicationQuit()
@@ -137,7 +139,7 @@ public class RPInputManager : MonoBehaviour
             try
             {
                 string line = sp.ReadLine();
-                Debug.Log(line);
+                // Debug.Log(line);
                 data = JsonUtility.FromJson<JsonData>(line);
             }
             catch
